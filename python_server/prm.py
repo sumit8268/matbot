e7fabba7a698c47e520a0dffc8af6fe75be92fe8
sumit8268 <sumitpatil0420@gmail.com>
2020-05-03 17:53:08 +0530

    added database tables User and Work

--- python_server/prm.py
+++ python_server/prm.py
@@ -2,10 +2,34 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+
+# adding object object to database
+# p1 = Point(point_label = "junction1", point_type = "junction", connected_to = None, with_color = None)
+# db.session.add(p1)
+# db.session.commit()
+
+
+
+# accessing data from android
+# request.form['source1']
+# request.form['dest1']
+
+
+# sending data to android
+# result = json.dumps({'success':'fafafafaf'})
+
+
+
+
+# flask object
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@192.168.43.170/matbot'
+
+# configuring sqlalchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/matbot'
 db = SQLAlchemy(app)
 
+
+# Point table
 class Point(db.Model):
     point_label =db.Column(db.String(100), primary_key=True)
     point_type = db.Column(db.String(100))
@@ -14,24 +38,34 @@ class Point(db.Model):
 
     def __repr__(self):
         return self.point_label
-    
 
 
-# p1 = Point(point_label = "junction1", point_type = "junction", connected_to = None, with_color = None)
-# db.session.add(p1)
-# db.session.commit()
+# User table
+class User(db.Model):
+    name =db.Column(db.String(100))
+    email = db.Column(db.String(100), primary_key=True)
+    password = db.Column(db.String(100))
+
+    def __repr__(self):
+        return self.email
+
+
+# Work table
+class Work(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    source =db.Column(db.String(100))
+    destination = db.Column(db.String(100))
+    work_done = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f"{self.source} -> {self.destination}"
+
 
 
 @app.route("/")
 def index():
     return "hello"
 
-@app.route("/addroute", methods=['POST', 'GET'])
-def object():
-    print(request.form['source1'],request.form['dest1'],request.form['color1'])
-    
-    j = json.dumps({'success':'fafafafaf'})
-    return j
 
 if __name__ == "__main__":
     app.run(host='192.168.43.170', port=5000, debug=False)
